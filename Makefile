68a02bc238da6a3b0ec8fac6d1ac45fa6cc676b5
Tom Legg <tjlegg@bombers.the-eleven.com>
2013-10-18 08:54:26 +0800

    Update to uwsgi 1.9.18.2

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #
 
 PORTNAME=	uwsgi
-PORTVERSION=	1.9.17.1
+PORTVERSION=	1.9.18.2
 CATEGORIES=	www python
 MASTER_SITES=	http://projects.unbit.it/downloads/
 
--- files/patch-plugins__python__uwsgiplugin.py
+++ files/patch-plugins__python__uwsgiplugin.py
@@ -1,6 +1,6 @@
---- ./plugins/python/uwsgiplugin.py.orig	2013-04-27 15:43:02.000000000 +0800
-+++ ./plugins/python/uwsgiplugin.py	2013-04-27 15:45:44.000000000 +0800
-@@ -22,37 +22,13 @@
+--- plugins/python/uwsgiplugin.py.orig	2013-10-17 02:58:20.000000000 +0800
++++ plugins/python/uwsgiplugin.py	2013-10-18 08:52:07.000000000 +0800
+@@ -18,45 +18,13 @@
  
  if not 'UWSGI_PYTHON_NOLIB' in os.environ:
      LIBS = sysconfig.get_config_var('LIBS').split() + sysconfig.get_config_var('SYSLIBS').split()
@@ -18,9 +18,17 @@
 -        # try 3.x style config dir
 -        if not os.path.exists(libdir):
 -            libdir = '%s/lib/python%s/config-%s' % (sys.prefix, version, get_python_version())
--        libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LDLIBRARY'))
--        if not os.path.exists(libpath): 
+-
+-        # get cpu type
+-        uname = os.uname()
+-        if uname[4].startswith('arm'):
 -            libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LIBRARY'))
+-            if not os.path.exists(libpath): 
+-                libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LDLIBRARY'))
+-        else:
+-            libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LDLIBRARY'))
+-            if not os.path.exists(libpath): 
+-                libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LIBRARY'))
 -        if not os.path.exists(libpath): 
 -            libpath = '%s/libpython%s.a' % (libdir, version)
 -        LIBS.append(libpath)
@@ -34,14 +42,15 @@
 -        except:
 -            LDFLAGS.append("-L%s/lib" % sysconfig.PREFIX)
 -            os.environ['LD_RUN_PATH'] = "%s/lib" % sysconfig.PREFIX
+-
+-        LIBS.append('-lpython%s' % get_python_version())
 +    try:
 +        LDFLAGS.append("-L%s" % sysconfig.get_config_var('LIBDIR'))
 +        os.environ['LD_RUN_PATH'] = "%s" % (sysconfig.get_config_var('LIBDIR'))
 +    except:
 +        LDFLAGS.append("-L%s/lib" % sysconfig.PREFIX)
 +        os.environ['LD_RUN_PATH'] = "%s/lib" % sysconfig.PREFIX
- 
--        LIBS.append('-lpython%s' % get_python_version())
++  
 +    LIBS.append('-lpython%s' % get_python_version())
  else:
      LIBS = []
